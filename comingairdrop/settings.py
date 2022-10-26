e60b057c2c5680f56f071bf9b573144843298cfb
Hoàng Anh Lâm <comficker@gmail.com>
2022-10-27 01:01:47 +0700

    Init

--- comingairdrop/settings.py
+++ comingairdrop/settings.py
@@ -147,8 +147,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'files/media')
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'files/')
 ]
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/files/static/'
+MEDIA_URL = '/files/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
