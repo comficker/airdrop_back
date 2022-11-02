5db3340790d47b87f5075bc3ae4c76233f7ee6ec
Hoàng Anh Lâm <comficker@gmail.com>
2022-11-03 00:53:51 +0700

    Init

--- apps/project/api/serializers.py
+++ apps/project/api/serializers.py
@@ -105,6 +105,7 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Event
         fields = [
+            "name",
             "id", "id_string", "is_joined", "is_following", "meta", "desc", "meta", "tasks", "project", "prizes",
             "date_start", "date_end"
         ]
